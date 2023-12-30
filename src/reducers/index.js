6280927076755129408363c9c147e717f92cef09
Erik Z <molagg92@gmail.com>
2023-12-30 06:22:25 -0800

    give passing green for root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
-// import formVisibleReducer from './form-visible-reducer';
-// import ticketListReducer from './ticket-list-reducer';
-// import { combineReducers } from 'redux';
+import formVisibleReducer from './form-visible-reducer';
+import ticketListReducer from './ticket-list-reducer';
+import { combineReducers } from 'redux';
 
-// const rootReducer = combineReducers({
-//   formVisibleOnPage: formVisibleReducer,
-//   mainTicketList: ticketListReducer
-// });
+const rootReducer = combineReducers({
+  formVisibleOnPage: formVisibleReducer,
+  mainTicketList: ticketListReducer
+});
 
-// export default rootReducer;
+export default rootReducer;

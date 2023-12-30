d236e07e1cfa89180345289ce733530063bd21b3
Erik Z <molagg92@gmail.com>
2023-12-30 14:15:34 -0800

    make root reducer for formvisible component

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -11,7 +11,7 @@ class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false,
+      // formVisibleOnPage: false,
       selectedTicket: null,
       editing: false
     };
@@ -19,14 +19,16 @@ class TicketControl extends React.Component {
     handleClick = () => {
     if (this.state.selectedTicket != null) {
       this.setState({
-        formVisibleOnPage: false,
+        // formVisibleOnPage: false,
         selectedTicket: null,
         editing: false
       });
     } else {
-      this.setState(prevState => ({
-        formVisibleOnPage: !prevState.formVisibleOnPage,
-      }));
+      const { dispatch } = this.props;
+      const action = {
+        type: 'TOGGLE_FORM'
+      }
+      dispatch(action);
     }
   }
     handleAddingNewTicketToList = (newTicket) => {
@@ -40,7 +42,10 @@ class TicketControl extends React.Component {
         issue: issue,
       }
       dispatch(action);
-      this.setState({formVisibleOnPage: false});
+      const action2 = {
+        type: 'TOGGLE_FORM'
+      }
+      dispatch(action2);
     }
 
     handleEditClick = () => {
@@ -93,7 +98,7 @@ class TicketControl extends React.Component {
                               onClickingEdit = {this.handleEditClick}/>
       buttonText = "Return to Ticket List";
     }
-    else if (this.state.formVisibleOnPage) {
+    else if (this.props.formVisibleOnPage) {
       currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList}  />;
       buttonText = "Return to Ticket List";
     } else {
@@ -117,7 +122,8 @@ TicketControl.propTypes = {
 
 const mapStateToProps = state => {
   return {
-    mainTicketList: state
+    mainTicketList: state.mainTicketList,
+    formVisibleOnPage: state.formVisibleOnPage
   }
 }
 
